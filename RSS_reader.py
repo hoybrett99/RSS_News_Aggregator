819ccb2dfba599a3c1f64c62b4fd9cac144945a6
Brett <148084194+hoybrett99@users.noreply.github.com>
2024-10-13 14:16:41 +0100

    Add files via upload

---
+++ RSS_reader.py
@@ -0,0 +1,54 @@
+import feedparser
+from flask import Flask, render_template, request
+from pyspark.sql.connect.functions import array_insert
+
+app = Flask(__name__)
+
+RSS_FEEDS = {
+    'Zambia Diggers': 'https://diggers.news/rss/',
+    'Kitwe News': 'https://kitweonline.com/feed',
+    'Daily Nations Zambia': 'https://dailynationzambia.com/feed/',
+    'Kitwe City Council': 'https://www.kitwecouncil.gov.zm/?feed=rss2',
+    'Lusaka Times': ' https://www.lusakatimes.com/feed/',
+    'ZNBC': 'https://znbc.co.zm/news/feed/'
+}
+
+
+@app.route('/')
+def index():
+    articles = []
+    for source, feed in RSS_FEEDS.items():
+        parsed_feed = feedparser.parse(feed)
+        entries = [(source, entry) for entry in parsed_feed.entries]
+        articles.extend(entries)
+
+    articles = sorted(articles, key=lambda x: x[1].published_parsed, reverse=True)
+
+    page = request.args.get('page', 1, type=int)
+    per_page = 50
+    total_articles = len(articles)
+    start = (page-1) * per_page
+    end = start + per_page
+    paginated_articles = articles[start:end]
+
+    return render_template('index.html', articles=paginated_articles, page=page,
+                           total_pages = total_articles // per_page + 1)
+
+
+@app.route('/search')
+def search():
+    query = request.args.get('q')
+
+    articles = []
+    for source, feed in RSS_FEEDS.items():
+        parsed_feed = feedparser.parse(feed)
+        entries = [(source, entry) for entry in parsed_feed.entries]
+        articles.extend(entries)
+
+    results = [article for article in articles if query.lower() in article[1].title.lower()]
+
+    return render_template('search_results.html', articles=results, query=query)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
